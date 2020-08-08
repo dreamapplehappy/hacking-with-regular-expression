e5a5c58bd19a6f7815436598265fe09312513ab7
dreamapplehappy <dreamapplehappy@gmail.com>
2020-08-08 17:16:08 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,26 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
+    title: '1234',
+    description: 'Just playing around',
+    themeConfig: {
+        displayAllHeaders: true,
+        logo: '/assets/img/logo.png',
+        nav: [
+            { text: 'demo', link: '' },
+            { text: 'demo', link: '' },
+        ],
+        sidebar: [
+            ['/common-regex-examples/', '常用的正则表达式'],
+            ['/learn-step-by-step/', '学习资料'],
+        ],
+        // github
+        repo: 'dreamapplehappy/hacking-with-regular-expression',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '帮助我们改善此页面！'
+    }
 }
